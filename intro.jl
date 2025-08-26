01fc3545184e872c7002a6e6a07a8bcba50d0ab1
dxtr0x <dextr.io@proton.me>
2025-08-26 11:33:37 +0000

    Add some comment about type and swapping

---
+++ intro.jl
@@ -0,0 +1,21 @@
+# Print text without new Line
+print("Hello World");
+
+# Print text with new Line
+println("Hello World with new Line");
+
+#=
+    This is Multi-Line comment
+=#
+
+# Print the type of variable
+typeof(7.0)
+
+#= Note :
+ - p,q = p,q => Swap variable
+ - p,q,r = 1,2,3 => Declare multiple variable
+ - x = 1;y = 2;z = 3; => Declare many variable in one Line
+ - <name>::<type> => Declare variable with special type
+ - const <name> = .... => Declare const variable (const in julia is mutiable in data but const in type) 
+
+=#
