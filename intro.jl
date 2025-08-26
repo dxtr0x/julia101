5468a3884d4c93cffdf7e9e601e57850a728af1b
dxtr0x <dextr.io@proton.me>
2025-08-26 13:53:17 +0000

    Add some concept in comment

--- intro.jl
+++ intro.jl
@@ -1,5 +1,6 @@
+println("============ Result ================");
 # Print text without new Line
-print("Hello World");
+print("Hello World\n");
 
 # Print text with new Line
 println("Hello World with new Line");
@@ -11,11 +12,14 @@ println("Hello World with new Line");
 # Print the type of variable
 typeof(7.0)
 
+
 #= Note :
+ - <name> = 
  - p,q = p,q => Swap variable
  - p,q,r = 1,2,3 => Declare multiple variable
  - x = 1;y = 2;z = 3; => Declare many variable in one Line
  - <name>::<type> => Declare variable with special type
+ - {expression}::<type> => Verify the Type's result of expression
  - const <name> = .... => Declare const variable (const in julia is mutiable in data but const in type) 
-
 =#
+
